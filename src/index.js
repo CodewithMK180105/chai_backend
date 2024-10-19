ad9c4da5c78c4fbcbfb2791b8d51eade3c633ccb
CodewithMK180105 <manishkumar180105@gmail.com>
2024-10-19 13:11:21 +0530

    successfully connected to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-
 // Note: Always use (try,catch) and (async,await);
+import dotenv from 'dotenv';
+dotenv.config({path: './env'})
 
-// function connectDB() {}
-// connectDB()
+import connectDB from "./db/index.js";
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
 
 (async ()=>{    //IIFE
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on('error',(error)=>{
+            console.log(error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
     }catch(error){
         console.error("Error connecting to MongoDB:", error.message);
         throw error;
     }
 })() 
+
+*/
